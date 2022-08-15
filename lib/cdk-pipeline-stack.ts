5f7ba70b0e3480c094f56398600478dbc29538bf
artemsavchuk94 <pro100artemqabin@gmail.com>
2022-08-15 15:07:46 +0300

    first commit

--- lib/cdk-pipeline-stack.ts
+++ lib/cdk-pipeline-stack.ts
@@ -1,16 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Stack, StackProps} from 'aws-cdk-lib';
+import {CodePipeline, CodePipelineSource, ShellStep} from "aws-cdk-lib/pipelines";
+import {GitHubTrigger} from "aws-cdk-lib/aws-codepipeline-actions";
+import {HostedZoneAttributes} from "aws-cdk-lib/aws-route53";
+//interface CdkPipelineStack extends StackProps {
+  //zoneAttrs: HostedZoneAttributes,
+  //githubBranch: string,
+  //isProduction?: boolean
+//}
 
 export class CdkPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const pipeline = new CodePipeline(this, 'pipeline', {
+      pipelineName: 'first-pipeline',
+      crossAccountKeys: false,
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('ArtemSavchuk94/AWS-First-CI-CD-Pipeline','main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+        additionalInputs: {
+          '../AWS-First-CI-CD-Pipeline': CodePipelineSource.gitHub('ArtemSavchuk94/AWS-First-CI-CD-Pipeline', 'main', {
+            trigger: GitHubTrigger.WEBHOOK
+          })
+        }
+      })
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkPipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
